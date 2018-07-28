e9a05590b2332bbfe5735bf8c712b32336bcbeec
Heber Teixeira <heber.navegante@gmail.com>
2018-07-28 11:38:23 -0300

    Cap03 - O começo da nossa aplicação

--- main.rb
+++ main.rb
@@ -3,5 +3,6 @@ require './loja_virtual/lib/livro'
 teste_e_design = Livro.new "Mauricio Aniche", "123454", 247, 60.9
 teste_e_design.preco = 89.9
 
+puts teste_e_design
 # puts teste_e_design.preco
-puts teste_e_design.instance_variable_get "@preco"
+# puts teste_e_design.instance_variable_get "@preco"
